69c806894b2b891d902560a6028c8bfac94e1e62
Erik Lövgren <zejrik5@yahoo.se>
2023-01-11 12:12:03 +0100

    2015 day 1 solution

---
+++ AoC2015/Program.cs
@@ -0,0 +1,28 @@
+﻿using AoC2015.Solutions;
+
+class Program
+{
+    static public void Main(string[] args)
+    {
+        //day1.SolveFirst();
+        day1.SolveSecond();
+        //day2.SolveFirst();
+        //day2.SolveSecond();
+        //day3.SolveFirst();
+        //day3.SolveSecond();
+        //day4.SolveFirst();
+        //day4.SolveSecond();
+        //day5.SolveFirst();
+        //day5.SolveSecond();
+        //day6.SolveFirst();
+        //day6.SolveSecond();
+        //day7.SolveFirst();
+        //day7.SolveSecond();
+        //day8.SolveFirst();
+        //day8.SolveSecond();
+        //day9.SolveFirst();
+        //day9.SolveSecond();
+        //day10.SolveFirst();
+        //day10.SolveSecond();
+    }
+}
---
+++ AoC2015/Solutions/day1.cs
@@ -0,0 +1,39 @@
+﻿namespace AoC2015.Solutions
+{
+    public class day1
+    {
+        private static readonly string input = File.ReadAllText(@"../../../Inputs/1.txt");
+
+        public static void SolveFirst()
+        {
+            var floor = 0;
+            foreach (var character in input)
+            {
+                if (character.ToString() == "(")
+                    floor++;
+
+                if (character.ToString() == ")")
+                    floor--;
+            }
+        }
+
+        public static void SolveSecond()
+        {
+            var floor = 0;
+            var position = 0;
+            foreach (var character in input)
+            {
+                if (character.ToString() == "(")
+                    floor++;
+
+                if (character.ToString() == ")")
+                    floor--;
+
+                position++;
+
+                if (floor == -1)
+                    break;
+            }
+        }
+    }
+}
