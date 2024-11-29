7dde54003742819cab4ef783cdc78befc69521c7
Ziviz <zejrik5@yahoo.se>
2024-11-29 21:35:02 +0100

    updated old .net versions. added project for 2024

---
+++ Aoc2024/Solutions/day1.cs
@@ -0,0 +1,19 @@
+﻿namespace Aoc2024.Solutions
+{
+    public class day1
+    {
+        public static void SolveFirst()
+        {
+            var inputRows = File.ReadAllLines(@"../../../Inputs/1.txt");
+
+           
+        }
+
+        public static void SolveSecond()
+        {
+            var inputRows = File.ReadAllLines(@"../../../Inputs/1.txt");
+
+            
+        }
+    }
+}
